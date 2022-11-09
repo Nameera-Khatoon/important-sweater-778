10b488a54c90ccf8f8f750c077d5ca7f9a1c2aa4
RishabhRai <rrishabhrai19105@gmail.com>
2022-11-09 17:33:06 +0530

    footer50

---
+++ scripts/footer.js
@@ -0,0 +1,39 @@
+let foot = `<div id="footer">
+<div class="d">
+    <p>Payment</p>
+    <img src="https://www.naturesbasket.co.in/Images/icons-card-payments-mode.png" alt="">
+</div>
+<div class="d">
+    <p>Keep in Touch</p>
+    <div id="icon">
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-youtube"></i>
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-pinterest-p"></i>
+    </div>
+</div>
+</div>
+
+<hr>
+<hr>
+<div id="l_footer">
+<img
+src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDYF4B8KRJNQZKBWPPaBOmwka-dTpQZfiEAQ&usqp=CAU"alt=""/>
+<p>2020 Natures Basket Limited. All rights reserved.</p>
+<hr />
+<p>Privacy Policy</p>
+<hr />
+<p>Terms of Use</p>
+<hr />
+<p>Home-Delivery Number - 888007745</p>
+<hr />
+<p>Site Map</p>
+<hr />
+<p>Mobile Site</p>
+<hr />
+<p>FSSAI License Number 11519002000496</p>
+</div>`
+
+let footer = document.querySelector('#footer')
+footer.innerHTML = foot;
